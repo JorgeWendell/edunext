9047145a8bd5b0e056a44b49e9dfa7455fe7f25e
JorgeWendell <jorge.wendell@adelbr.tech>
2025-12-19 18:03:39 -0300

    iniciandooo

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Turbopack desabilitado - usando Webpack padrão
 };
 
 export default nextConfig;
